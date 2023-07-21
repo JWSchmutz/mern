a1bca8e16f52c4c48ed43589f2d06bb65fd79157
jwschmutz <jeremiah.schmutz@gmail.com>
2023-07-21 00:21:10 -0400

    trying new root

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "./client/dist/")));
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile("main.html", { root: "public" });
 });
 
 app.get("/addresses", async (req, res) => {
