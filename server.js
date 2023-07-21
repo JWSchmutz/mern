5a4e53cd10ff94620d64c7c3d1a837ad4e8cd9a8
jwschmutz <jeremiah.schmutz@gmail.com>
2023-07-21 00:23:24 -0400

    reverting

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const axios = require("axios");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-const path = require("path");
+
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "./client/dist/")));
-app.get("*", (req, res) => {
-  res.sendFile("index.html", { root: "public" });
+app.use(express.static("client"));
+
+app.get("/", async (req, res) => {
+  // Make a request for a user with a given ID
+  res.sendFile("dist/index.html", { root: __dirname });
 });
 
 app.get("/addresses", async (req, res) => {
